4e5c70eb31d2c186edbc638b4f6dea2d24a9693a
olikbarabolik <62029315+olikbarabolik@users.noreply.github.com>
2020-05-09 23:03:17 +0400

    +RestAssured

--- rest/src/test/java/ru/stqa/pft/resttests/RestAssuredTests.java
+++ rest/src/test/java/ru/stqa/pft/resttests/RestAssuredTests.java
@@ -1,8 +1,9 @@
 
 package ru.stqa.pft.rest;
-
+import com.jayway.restassured.RestAssured;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
 import  org.apache.http.client.fluent.Request;
 import org.apache.http.client.fluent.Response;
 import java.io.IOException;
@@ -22,10 +23,15 @@ import org.apache.http.client.fluent.Executor;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.NameValuePair;
 
-public class RestTests {
+public class RestAssuredTests {
+
+    @BeforeClass
+    public void init(){
+        RestAssured.authentication= RestAssured.basic("288f44776e7bec4bf44fdfeb1e646490","");
+    }
 
     @Test
-    public void testCreateIssue() throws IOException{
+    public void testCreateIssueAssured() throws IOException{
         Set<Issue> oldIssues = getIssues();
         Issue newIssue = new Issue().withSubject("test rest issue!!!").withDescription("test new description!");
         int issueId = createIssue(newIssue);
@@ -36,22 +42,23 @@ public class RestTests {
 
     }
     private int createIssue(Issue newIssue) throws IOException {
-        String json = getExecutor().execute(Request.Post("https://bugify.stqa.ru/api/issues.json?limit=500")
-                .bodyForm(new BasicNameValuePair("subject", newIssue.getSubject()),
-                        new BasicNameValuePair("description", newIssue.getDescription())))
-                .returnContent().asString();
+        String json = RestAssured.given()
+                .parameter("subject", newIssue.getSubject())
+                .parameter("description", newIssue.getDescription())
+                .post("https://bugify.stqa.ru/api/issues.json?limit=500").asString();
+
+
         JsonElement parsed = new JsonParser().parse(json);
         return parsed.getAsJsonObject().get("issue_id").getAsInt();
+        //return 0;
     }
 
     private Set<Issue> getIssues() throws IOException {
-        String json = getExecutor().execute(Request.Get("http://bugify.stqa.ru/api/issues.json?limit=500")).returnContent().asString();;
+
+        String json = RestAssured.get("http://bugify.stqa.ru/api/issues.json?limit=500").asString();
         JsonElement parsed = new JsonParser().parse(json);
         JsonElement issues = parsed.getAsJsonObject().get("issues");
         return new Gson().fromJson(issues, new TypeToken<Set<Issue>>(){}.getType());
     }
 
-    private Executor getExecutor() {
-        return Executor.newInstance().auth("288f44776e7bec4bf44fdfeb1e646490","");
-    }
 }
---
+++ soap-ui/src/test/java/ru/stqa/pft/soap-ui/TestSoap.java
@@ -0,0 +1,12 @@
+package ru.stqa.pft.soap-ui;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Task3 {
+
+    @Test
+    public void correctTest(){
+
+    }
+}
