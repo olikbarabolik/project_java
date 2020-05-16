81b61b3401e3c2879b879ed5f5e028babaf20cd9
olikbarabolik <62029315+olikbarabolik@users.noreply.github.com>
2020-05-16 12:33:51 +0400

    + win7

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -63,7 +63,7 @@ public class ApplicationManager {
         }  else{
             DesiredCapabilities capabilites = new DesiredCapabilities();
             capabilites.setBrowserName(browser);
-            capabilites.setPlatform(Platform.fromString(System.getProperty("platform","VISTA")));
+            capabilites.setPlatform(Platform.fromString(System.getProperty("platform","win7")));
             wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilites);
         }
 
